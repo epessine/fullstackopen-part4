207161ec0840e2b5e20a42a677fe685161b42d9d
Eduardo Pessine <eduardopessine@gmail.com>
2021-04-04 21:45:54 -0300

    Exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
+const dummy = blogs => {
   return blogs.reduce(out => out, 1);
 };
 
+const totalLikes = blogs => {
+  const total = blogs.reduce((likes, blogs) => {
+    return likes + blogs.likes;
+  }, 0);
+
+  return blogs.length === 0 ? 0 : total;
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
